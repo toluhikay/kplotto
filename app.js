8de3a111954dd4a909df6edd8fa15257a7a7cf81
toluhikay <78743753+toluhikay@users.noreply.github.com>
2021-10-12 00:29:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-let kay = document.getElementsByClassName("logo");
+const test = document.querySelectorAll(".draw-dates");
 
+test.forEach((cur) => {
+  cur.addEventListener("click", (event) => {
+    const selected_draw = event.currentTarget.closest("div").childNodes[3];
+
+    selected_draw.classList.toggle("active");
+  });
+});
+
+let nav = document.querySelector(".hamburger-menu");
+let nextHeader = document.querySelector(".next-header");
+
+
+nav.addEventListener("click", ()=>{
+    nextHeader.classList.toggle("slide");
+});
